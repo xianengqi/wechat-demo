b641bb95beedcbac2dc72935b15e73e9380bab1d
xianengqi <920986617@qq.com>
2019-01-27 08:38:09 +0800

    request api update

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  version: "1.0",
+  note: '优化接口调用流程',
+  subDomain: "tz", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd
+  appid: "wx18242538b806569b", // 你的小程序的appid, 购物单功能需要使用
+  shareProfile: '百款精品商品，总有一款适合你' // 首页转发的时候话术
+}
